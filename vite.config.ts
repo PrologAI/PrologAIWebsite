8a2bd382ac4970187baa92f13fee23291c3daf68
Mohamed Yusuf <43543845+mohdgazyanii235@users.noreply.github.com>
2025-11-08 05:33:48 +0000

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
   //   host: "::",
   //   port: 8080,
   // },
-  // base: '/',
+  base: 'PrologAIWebsite/',
   plugins: [
     react(),
     mode === 'development' &&
