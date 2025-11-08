5a24b936e83c5d5dd7a0d8527a85526d84e4a483
Mohamed Yusuf <43543845+mohdgazyanii235@users.noreply.github.com>
2025-11-08 05:23:10 +0000

    Set base path for PrologAIWebsite

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
   //   host: "::",
   //   port: 8080,
   // },
+  base: '/PrologAIWebsite/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
